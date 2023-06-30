9d3c2951587c64c6ba2f16916f34131c176ba8c2
Aphelele Joyi <aphelelejoyi@gmail.com>
2023-07-01 00:00:09 +0200

    projects and testimonials done

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,45 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+
+
+const dataUrl = "https://aphelele2.github.io/eompJson/data/";
 
 export default createStore({
   state: {
+    projects: null,
+    testimonials: null
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    setProjects(state, projects) {
+      state.projects = projects;
+    },
+    
+    setTestimonials(state, testimonials){
+      state.testimonials=testimonials
+    }
+    
   },
   actions: {
+    async fetchProjects(context) {
+      try {
+        let res = await fetch(dataUrl);
+        let { projects } = await res.json();
+        context.commit('setProjects', projects);
+      } catch (e) {
+        console.log(e.message);
+      }
+    },
+    async fetchTestimonials(context) {
+      try {
+        let res = await fetch(dataUrl);
+        let { testimonials } = await res.json();
+        context.commit('setTestimonials', testimonials);
+      } catch (e) {
+        console.log(e.message);
+      }
+    }
   },
   modules: {
+
   }
-})
+});
