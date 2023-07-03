bb2118958082b5c6c0dd7dbc6641d121e72892ed
Aphelele Joyi <aphelelejoyi@gmail.com>
2023-07-03 03:10:10 +0200

    well done

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,29 +1,108 @@
 <template>
-    <div>
-       <h1>Contact</h1>
-       <div  class="container mb-4 ">
+  <div class="background">
+    <h1 class="heading"><span class="highlight">Connect</span> with me</h1>
+    <div class="container">
+      <div class="form-container">
         <form action="https://formspree.io/f/xgebdjdv" method="post">
           <label for="fname">First Name</label>
-          <input type="text" id="fname" name="firstname" placeholder="Please enter your name">
+          <input type="text" id="fname" name="firstname" placeholder="Enter name">
           <label for="lname">Last Name</label>
-          <input type="text" id="lname" name="lastname" placeholder="Please enter your last name">
-          <label for="subject">Email address</label>
-          <input type="text" id="ename" name="email_address" placeholder="Please enter your email address">
+          <input type="text" id="lname" name="lastname" placeholder="Enter surname">
+          <label for="email">Email address</label>
+          <input type="text" id="email" name="email_address" placeholder="Enter email address">
           <label for="subject">Message</label>
-          <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
-          <input type="submit" value="Submit">
-          <br><br><br><br><br>
+          <textarea id="subject" name="subject" placeholder="Write something.." style="height: 200px;"></textarea>
+          <input type="submit" value="Submit" class="submit-button">
         </form>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+  
+}
 </script>
 
 <style scoped>
+.background {
+  background-image: url('https://i.postimg.cc/gkYYd3H3/website-internet-contact-us-page-concept-with-phone-chat-email-icons-symbol-telephone-mail-mobile-ph.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
 
+.container {
+  width: 50%;
+  margin: 0 auto;
+}
+
+.heading {
+  text-align: center;
+  margin-top: 20px;
+  animation: fade-in 1s ease-in-out;
+  text-decoration: underline;
+  font-weight: bold;
+  padding-top: 70px; 
+}
+
+
+.highlight {
+  color: rgb(55, 55, 153);
+  font-weight: bold;
+}
+
+@keyframes fade-in {
+  0% {
+    opacity: 0;
+    transform: translateY(-20px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+.form-container {
+  margin-top: 50px;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+label {
+  margin-bottom: 8px;
+  font-size: 20px; 
+  font-weight: bold; 
+  color: #3c2c2c;
+  text-decoration: underline dotted;
+}
+
+input[type="text"],
+textarea {
+  width: 100%;
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-bottom: 16px;
+  resize: vertical;
+}
+
+.submit-button {
+  background-color: darkslategrey;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  width: auto;
+}
+
+.submit-button:hover {
+  background-color: darkslategray;
+}
 </style>
--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -1,30 +1,36 @@
 <template>
-  <div> 
-          <section class="col">
+    <h1 class="head">Education | Experience</h1>
+  <div class="main-content">
+    <section class="col">
       <header class="title">
         <h2>Education</h2>
       </header>
-      <div class="container" v-for="edu in Education" :key="edu.id" id="box">
-        <h4>{{ edu.year }}</h4>
-        <h3>{{ edu.title }}</h3>
-        <p>{{ edu.description }}</p>
+      <div class="content">
+        <div v-for="edu in Education" :key="edu.id" class="box">
+          <h4>{{ edu.year }}</h4>
+          <h3>{{ edu.title }}</h3>
+          <p>{{ edu.description }}</p>
+        </div>
       </div>
     </section>
-    
+
     <section class="col">
       <header class="title">
         <h2>Work and Experience</h2>
       </header>
-      <div class="contents">
-        <div class="container" v-for="exp in Experience" :key="exp.id" id="box">
+      <div class="content">
+        <div v-for="exp in Experience" :key="exp.id" class="box">
           <h4>{{ exp.year }}</h4>
           <h3>{{ exp.title }}</h3>
           <p>{{ exp.description }}</p>
         </div>
       </div>
     </section>
+  </div>
 
-   <div class="container">
+  <div class="container">
+    <h1>Skill Set</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ut eveniet sapiente dolore omnis dolorem, dignissimos, nostrum quasi, aperiam exercitationem saepe eius! Laudantium sapiente consequuntur mollitia nemo facere reprehenderit dicta.</p>
     <div class="row">
       <div class="col-lg-4" v-for="skill in skills" :key="skill.id">
         <div class="card mb-4">
@@ -41,10 +47,8 @@
       </div>
     </div>
   </div>
-  </div>  
-          
 </template>
-      
+
 <script>
 export default {
   computed: {
@@ -61,9 +65,9 @@ export default {
   mounted() {
     this.$store.dispatch('fetchEducation');
     this.$store.dispatch('fetchExperience');
-     this.$store.dispatch('fetchSkills');
+    this.$store.dispatch('fetchSkills');
   },
-  methods :{
+  methods: {
     truncateText(text) {
       const maxLength = 100;
       if (text.length > maxLength) {
@@ -79,36 +83,77 @@ export default {
 </script>
 
 <style scoped>
-.main-content{
-    display: flex;
+
+/* ============================================== EDUCATION AND EXPERIENCE =========================================== */
+.main-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80%;
+  margin: 0 auto;
+  padding-top: 50px;
 }
-.main-content #box{
-    flex-basis: 50%;
-    padding: 10px 20px;
+.head{
+  
+  text-align: center;
+   padding-top: 100px;
 }
-#box h2{
-    font-size: 1.12rem;
-    font-weight: 600;
-    text-transform: uppercase;
+
+.main-content .col {
+  flex-basis: 50%;
+  padding: 10px 20px;
+  border-radius: 8px;
+  background-color: #f5f5f5;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 }
-#box .content{
-    margin: 20px 0 0 0;
+
+.main-content h2 {
+  font-size: 1.2rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  margin-bottom: 10px;
+  color: #333;
+}
+
+.content {
+  margin: 20px 0 0 0;
+}
+
+.box {
+  margin-bottom: 20px;
+}
+
+.box h4 {
+  font-size: 1rem;
+  font-weight: 600;
+  margin-bottom: 5px;
+  color: #555;
+}
+
+.box h3 {
+  font-size: 1.1rem;
+  font-weight: 500;
+  margin-bottom: 5px;
+  color: #333;
+}
+
+.box p {
+  font-size: 0.9rem;
+  color: #777;
 }
 
 
-
-
-.d-flex h1{
-    margin-right: 5rem;
-    margin-top: 3rem;
-
-}
+/* ======================================= SKILLS  ==================================================== */
 .container {
   padding-top: 2rem;
 }
 
 .card {
   width: 100%;
+  border-radius: 8px;
+  overflow: hidden;
+  background-color: #fff;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 }
 
 .card-img-top {
@@ -116,13 +161,47 @@ export default {
   object-fit: cover;
 }
 
-.btn {
-  margin-top: 1rem;
+.card-body {
+  padding: 15px;
 }
 
+.card-title {
+  font-size: 1.1rem;
+  font-weight: 600;
+  margin-bottom: 10px;
+  color: #333;
+}
+
+.card-text {
+  font-size: 0.9rem;
+  color: #777;
+}
+
+.btn {
+  margin-top: 1rem;
+  font-size: 0.9rem;
+  font-weight: 500;
+}
+
+.btn-primary {
+  background-color: #007bff;
+  border-color: #007bff;
+}
+
+.btn-primary:hover {
+  background-color: #0069d9;
+  border-color: #0062cc;
+}
+
+.btn-primary:focus,
+.btn-primary.focus {
+  box-shadow: 0 0 0 0.2rem rgba(38, 143, 255, 0.5);
+}
+
+
+
+
+
+
+
 </style>
-          
-          
-        
-          
-          
