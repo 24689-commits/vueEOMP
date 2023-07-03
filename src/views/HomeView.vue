e2d718aeedc4ff05d6b30b7c988729006c69b45e
Aphelele2 <aphelelejoyi@gmail.com>
2023-07-03 16:37:49 +0200

    home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,40 @@
 <template>
-  <div class="home">
-     <h1 class="head">Home</h1>
+  <div class="home align-center">
+    <div class="card" style="max-width: 100%; max-height:100%">
+      <div class="row g-0">
+        <div class="col">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+        <div class="col">
+          <img src="https://i.postimg.cc/KjnTKFVD/Thotyelwa-Mpongwana.jpg" class="img-fluid img-top" alt="...">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
-
 export default {
   name: 'HomeView',
-  components: {
-    
-  }
-}
+  components: {},
+};
 </script>
 
 <style scoped>
-.head{
+.align-center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.head {
   margin-top: 80px;
+}
+.home {
+  margin-top: 56px;
 }
 </style>
